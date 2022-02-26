28326013e645ff04559d6bae5388231e4dc83c41
Aidan Wright <49991562+theoneGUI@users.noreply.github.com>
2022-02-25 19:47:26 -0600

    Updating files and adding additional functionality

---
+++ SUAPI/ErrorCheck.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyncUpAPI.Controllers {
+    [ApiController]
+    [Route("/error/errorCheck")]
+    public class ErrorCheck : ControllerBase {
+        [HttpGet]
+        public string Get(string sessID, string status, string data)
+        {
+            if (status.Equals("GET"))
+            {
+                if (Universe.errors.ContainsKey(sessID))
+                {
+                    return Universe.errors[sessID];
+                }
+                else
+                {
+                    return "key_not_available";
+                }
+            }
+            else if (status.Equals("SET"))
+            {
+                try
+                {
+                    if (Universe.openRequests.ContainsKey(sessID))
+                    {
+                        Universe.errors[sessID] = data;
+                        return "set_success";
+                    }
+                    else
+                        return "matchmaking_key_does_not_exist";
+                }
+                catch (Exception)
+                {
+                    return "set_error";
+                }
+            }
+            else
+            {
+                return "not_valid_operation";
+            }
+        }
+
+
+    }
+}
